abb7ce61be7221565af9d830309a1d52d735c331
ASUS <nam140103@gmail.com>
2024-04-09 11:14:32 +0700

    Hello C

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/chapter03/sum&avg.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/untitled.dir/chapter03/sum&avg.c.obj: \
+ C:/Users/ASUS/CLionProjects/untitled/chapter03/sum&avg.c \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ D:/Clion/CLion\ 2023.3.4/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h
