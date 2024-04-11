eb750420e915ebfab542ced1eaa06a9f32d81d4c
Trinh Ngoc Hoang Nam <nam140103@gmail.com>
2024-04-11 20:17:56 +0700

    tinhbieuthuc.c

---
+++ session6/tinhbieuthuc.c
@@ -0,0 +1,9 @@
+//
+// Created by ASUS on 4/11/2024.
+//
+#include "stdio.h"
+#include "math.h"
+//int tinhbieuthuc(){
+    int main() {
+    printf("result = %lld", 10 * (int)pow(3,6) * 6 + 5 - 2 && (2 * 2 + 6 / 3 > 1 || 2 > 8));
+}
