1b3f9c0a09e12ced42b18a344fa709da1267fcc7
Trinh Ngoc Hoang Nam <nam140103@gmail.com>
2024-04-11 23:54:56 +0700

    tinhbieuthuc.c

---
+++ session5/bang luong.c
@@ -0,0 +1,25 @@
+//
+// Created by ASUS on 4/11/2024.
+//
+#include "stdio.h"
+//int bangluong(){
+int main(){
+    int basic, daper;
+    float bonper, loanet;
+    char name[10];
+//    salary = basic + basic * daper / 100 + bonper * basic / 100 - loanet
+    printf("Ten cua ban: ");
+    scanf("%[^\n]s", &name);
+    printf("Basic:$ ");
+    scanf("%d", &basic);
+    printf("Daper:$ ");
+    scanf("%d", &daper);
+    printf("Bonper:$ ");
+    scanf("%f", &bonper);
+    printf("Loanet:$ ");
+    scanf("%f", &loanet);
+    float salary = basic + basic * daper / 100 + bonper * basic / 100 - loanet;
+    printf("==============Bang luong==============\n");
+    printf("Name:%s \tBasic:$%d \tSalary:$%f", name, basic, salary);
+
+}
