ff4f05d66ee0fb06374d79f59b4f792810bc7a6e
Trinh Ngoc Hoang Nam <nam140103@gmail.com>
2024-04-16 23:02:13 +0700

    bai tap

---
+++ session8/tu_luyen.c
@@ -0,0 +1,26 @@
+//
+// Created by ASUS on 4/16/2024.
+//
+#include "stdio.h"
+//int tu_luyen(){
+int main(){
+    int select, b, c;
+    printf("Chao mung ban de voi le hoi tinh yeu\n");
+    printf("Ban co nguoi yeu chua?\n");
+    printf("1. Toi chua\n");
+    printf("2. Toi co roi\n");
+    scanf("%d", &select);
+    switch (select) {
+        case 1:
+            printf("Vay kiem ny di nha hahahaha");
+            break;
+        case 2:
+            printf("Xin chuc mung!!!");
+            break;
+
+    }
+
+
+
+
+}
