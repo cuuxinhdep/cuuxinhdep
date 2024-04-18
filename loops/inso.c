feb540826ef9c68ba832d834c08b4b06f8dd4302
Trinh Ngoc Hoang Nam <nam140103@gmail.com>
2024-04-18 23:33:08 +0700

    bai tap

---
+++ loops/inso.c
@@ -0,0 +1,12 @@
+//
+// Created by ASUS on 4/18/2024.
+//
+#include "stdio.h"
+//int inso(){
+int main(){
+    int a = 100;
+    for (int i = 0; i <=  19; i ++) {
+        printf("%d ", a);
+        a-=5;
+    }
+}
