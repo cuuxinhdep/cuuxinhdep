425106033cadd4d7b5ac9da5bc8e5aeeb9043ca0
Trinh Ngoc Hoang Nam <nam140103@gmail.com>
2024-05-02 20:19:46 +0700

    bai tap

---
+++ array/Heron.c
@@ -0,0 +1,26 @@
+//
+// Created by ASUS on 5/2/2024.
+//
+#include "stdio.h"
+#include "math.h"
+//int heron(){
+//int getarea(int a, int b, int c){
+//    int p = (a + b + c) / 2;
+//    return sqrt(p * (p - a) * (p - b) * (p - c)) ;
+//}
+//
+//int main(){
+//int a, b, c;
+//    printf("Nhap canh thu 1:");
+//    scanf("%d", &a);
+//    printf("Nhap canh thu 2:");
+//    scanf("%d", &b);
+//    printf("Nhap canh thu 3:");
+//    scanf("%d", &c);
+//if(a + b > c && a + c > b && b + c > a){
+//    printf("Dien tich tam giac la: %d", getarea(a,b,c));
+//} else{
+//    printf("INVALID!!");
+//}
+//
+//}
---
+++ array/array/ktraboiso.c
@@ -0,0 +1,25 @@
+//
+// Created by ASUS on 5/2/2024.
+//
+#include "stdio.h"
+//int ktraboiso(){
+//void array(int n, int a[]){
+//    for (int i = 0; i < n; ++i) {
+//        scanf("%d", &a[i]);
+//    }
+//}
+//
+//int check(int a){
+//    if( a % 50 == 0) return 0;
+//    else return 1;
+//}
+//
+//int main(){
+//    int n;
+//    scanf("%d", &n);
+//    int a[n];
+//    array(n,a);
+//    for (int i = 0; i < n; ++i) {
+//        if(!check(a[i])) printf("%d ", a[i]);
+//    }
+//}
